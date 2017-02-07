7402f9a672c7e91bc004093807f83778cea3440d
Dale Cameron <dalecorp@hotmail.com>
2017-02-07 00:23:55 -0600

    Initial Project Setup

---
+++ OrderViewer.Database/dbo/Tables/Order.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Order] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [ClientId]    INT            NOT NULL,
+    [ClientName]  NVARCHAR (150) NOT NULL,
+    [OrderDate] DATETIME       NOT NULL,
+    [GST]         SMALLMONEY     NOT NULL,
+    [Total]       MONEY          NOT NULL
+);
+
+
+
---
+++ OrderViewer.Database/dbo/Tables/OrderItem.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[OrderItem] (
+    [Id]            INT           IDENTITY (1, 1) NOT NULL,
+    [ProductCode]   NVARCHAR (50) NOT NULL,
+    [Quantity]      INT           NOT NULL,
+    [TotalPrice]    MONEY         NOT NULL,
+    [ParentOrder] INT           NOT NULL
+);
+
